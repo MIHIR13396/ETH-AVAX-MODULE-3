52b2fa1f0008aa51b99fc8b2e3a5caf04024115c
MIHIR13396 <mihirsinghofficial10@gmail.com>
2024-06-25 08:59:52 +0530

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.0;
 
 contract MyUniqueToken {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
+    string public name = "XeronToken";
+    string public symbol = "XRN";
+    uint8 public decimals = 12;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     address public owner;
@@ -14,26 +14,7 @@ contract MyUniqueToken {
     event Burn(address indexed from, uint256 value);
 
     constructor() {
-        name = "XeronToken";
-        symbol = "XRN";
-        decimals = 12;
-        totalSupply = 1000 * 10**uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;
         owner = msg.sender;
-
-  
-        address[] memory addresses = new address[](3);
-        addresses[0] = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        addresses[1] = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-        addresses[2] = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-        uint256 tokensPerAddress = 1000 * 10**uint256(decimals);
-
-        for (uint256 i = 0; i < addresses.length; i++) {
-            balanceOf[addresses[i]] += tokensPerAddress;
-            totalSupply += tokensPerAddress;
-            emit Transfer(address(0), addresses[i], tokensPerAddress);
-            emit Mint(addresses[i], tokensPerAddress);
-        }
     }
 
     modifier onlyOwner() {
@@ -60,8 +41,10 @@ contract MyUniqueToken {
     function transfer(address _to, uint256 _amount) public {
         require(_to != address(0), "Invalid address");
         require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
-        balanceOf[msg.sender] -= _amount;
+        balanceOf[msg.sender] -= _amount;                         
+        // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 (Trasfer token to this address)
         balanceOf[_to] += _amount;
         emit Transfer(msg.sender, _to, _amount);
-    }
+    }                                                                          
 }
+
