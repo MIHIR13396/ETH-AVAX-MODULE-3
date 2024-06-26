f4eda329adce91d8c85b900bc790be0503980371
MIHIR13396 <mihirsinghofficial10@gmail.com>
2024-06-26 09:12:26 +0530

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,50 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
-contract MyUniqueToken {
-    string public name = "XeronToken";
-    string public symbol = "XRN";
-    uint8 public decimals = 12;
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    address public owner;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Mint(address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
-
-    constructor() {
-        owner = msg.sender;
+contract XeronToken is ERC20, Ownable {
+    constructor() ERC20("XeronToken", "XRN") Ownable(msg.sender) {
+        // Initial minting can be done here if needed, for example:
+        // _mint(msg.sender, initialSupply);
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function");
-        _;
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Invalid address");
+        _mint(to, amount);
     }
 
-    function mint(address _to, uint256 _amount) public onlyOwner {
-        require(_to != address(0), "Invalid address");
-        totalSupply += _amount;
-        balanceOf[_to] += _amount;
-        emit Mint(_to, _amount);
-        emit Transfer(address(0), _to, _amount);
+    function burn(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
     }
-
-    function burn(uint256 _amount) public {
-        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
-        balanceOf[msg.sender] -= _amount;
-        totalSupply -= _amount;
-        emit Burn(msg.sender, _amount);
-        emit Transfer(msg.sender, address(0), _amount);
-    }
-
-    function transfer(address _to, uint256 _amount) public {
-        require(_to != address(0), "Invalid address");
-        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
-        balanceOf[msg.sender] -= _amount;                         
-        // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 (Trasfer token to this address)
-        balanceOf[_to] += _amount;
-        emit Transfer(msg.sender, _to, _amount);
-    }                                                                          
 }
+  // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 (Trasfer token to this address)
 
